b28e8ccad38715c216808d56b47f3893c7ab1159
Xinxing Liu <xinxingliu86@gmail.com>
2026-07-06 19:54:20 +0000

    optimize with sorted list

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -1,8 +1,60 @@
 
 import os
 from collections import Counter
+from sortedcontainers import SortedList
+import time
+import pickle
 
 from cs336_basics.pretokenization import count_pretokens
+
+
+class PairMap:
+    def __init__(self):
+        self._lookup: dict[tuple[bytes, bytes], list[int, list[int]]] = {}
+        self._sorted_items = SortedList(
+            key=lambda item: (item[1][0], item[0]))
+
+    def add_pair_count(self, pair: tuple[bytes, bytes], pretoken_count: int, pretoken_index: int):
+        if pair not in self._lookup:
+            value = [0, []]
+            self._lookup[pair] = value
+            self._sorted_items.add((pair, value))
+
+        value = self._lookup[pair]
+        self._sorted_items.remove((pair, value))
+
+        value[0] += pretoken_count
+        if (len(value[1]) == 0 or value[1][-1] != pretoken_index):
+            value[1].append(pretoken_index)
+
+        self._lookup[pair] = value
+        self._sorted_items.add((pair, value))
+
+    def reduce_pair_count(self, pair: tuple[bytes, bytes], pretoken_count: int):
+        assert pair in self._lookup, "missing pair I"
+        value = self._lookup[pair]
+        self._sorted_items.remove((pair, value))
+
+        value[0] -= pretoken_count
+
+        self._lookup[pair] = value
+        self._sorted_items.add((pair, value))
+
+        if (value[0] == 0):
+            self._lookup.pop(pair)
+            self._sorted_items.remove((pair, value))
+
+    def get_next_pair(self) -> tuple[tuple[bytes, bytes], int, list[int]]:
+        (pair, [count, pretoken_indices]) = self._sorted_items[-1]
+        return (pair, count, pretoken_indices)
+
+    def remove_next_pair(self, pair: tuple[bytes, bytes]):
+        assert pair in self._lookup, "missing pair II"
+        value = self._lookup.pop(pair)
+        self._sorted_items.remove((pair, value))
+
+    def has_next_pair(self) -> bool:
+        return len(self._sorted_items) > 0
 
 
 class MergeManager:
@@ -13,39 +65,19 @@ class MergeManager:
             elements = [bytes([b]) for b in pretoken.encode("utf-8")]
             self.pretoken_elements_and_counts.append((elements, count))
 
-        self.pairs: dict[tuple[bytes, bytes], list[int, list[int]]] = {}
+        self.pairs: PairMap = PairMap()
 
         for i in range(len(self.pretoken_elements_and_counts)):
             (elements, pretoken_count) = self.pretoken_elements_and_counts[i]
             for j in range(len(elements) - 1):
-                self._add_pair_count((elements[j], elements[j + 1]), pretoken_count, i)
-
-        # print(self.pretoken_elements_and_counts)
-        # print(self.pairs)
-
-    def _add_pair_count(self, pair: tuple[bytes, bytes], pretoken_count: int, pretoken_index: int):
-        pair_entry = self.pairs.setdefault(pair, [0, []])
-        pair_entry[0] += pretoken_count
-        if (len(pair_entry[1]) == 0 or pair_entry[1][-1] != pretoken_index):
-            pair_entry[1].append(pretoken_index)
-    
-    def _reduce_pair_count(self, pair: tuple[bytes, bytes], pretoken_count: int):
-        assert pair in self.pairs, "missing pair"
-        pair_entry = self.pairs[pair]
-        pair_entry[0] -= pretoken_count
-        if (pair_entry[0] == 0):
-            del self.pairs[pair]
+                self.pairs.add_pair_count(
+                    (elements[j], elements[j + 1]), pretoken_count, i)
 
     def has_next(self) -> bool:
-        return len(self.pairs) > 0
+        return self.pairs.has_next_pair()
 
     def next(self) -> tuple[tuple[bytes, bytes], bytes]:
-        (merge, [count, pretoken_indices]) = ((b"", b""), [0, []])
-        for p, e in self.pairs.items():
-            if e[0] > count or (e[0] == count and p > merge):
-                merge = p
-                count = e[0]
-                pretoken_indices = e[1]
+        (merge, count, pretoken_indices) = self.pairs.get_next_pair()
 
         (a, b) = merge
         token = a + b
@@ -54,7 +86,6 @@ class MergeManager:
         for pretoken_index in pretoken_indices:
             (pretoken_elements,
              pretoken_count) = self.pretoken_elements_and_counts[pretoken_index]
-            # for j in range(len(pretoken_elements) - 1):
             j = -1
             while (True):
                 j += 1
@@ -69,15 +100,22 @@ class MergeManager:
                 del pretoken_elements[j+1]
                 # 2. add new pairs: (x, a, b, y) => (x, ab, y)
                 if (j > 0):
-                    self._add_pair_count((pretoken_elements[j-1], token), pretoken_count, pretoken_index)
-                    self._reduce_pair_count((pretoken_elements[j-1], a), pretoken_count)
+                    self.pairs.add_pair_count(
+                        (pretoken_elements[j-1], token), pretoken_count, pretoken_index)
+                    self.pairs.reduce_pair_count(
+                        (pretoken_elements[j-1], a), pretoken_count)
                 if (j + 1 < len(pretoken_elements)):
-                    self._add_pair_count((token, pretoken_elements[j+1]), pretoken_count, pretoken_index)
-                    self._reduce_pair_count((b, pretoken_elements[j+1]), pretoken_count)
+                    self.pairs.add_pair_count(
+                        (token, pretoken_elements[j+1]), pretoken_count, pretoken_index)
+                    self.pairs.reduce_pair_count(
+                        (b, pretoken_elements[j+1]), pretoken_count)
 
         # print(self.pretoken_elements_and_counts)
-        del self.pairs[merge]
+        self.pairs.remove_next_pair(merge)
+        # print(self.pairs._lookup)
+        # print(self.pairs._sorted_items)
         return (merge, token)
+
 
 def build_vocab(
     merge_manager: MergeManager,
@@ -102,6 +140,7 @@ def build_vocab(
 
     return (vocab, merges)
 
+
 def train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
@@ -114,17 +153,7 @@ def train_bpe(
     return build_vocab(merge_manager, vocab_size, special_tokens)
 
 
-if __name__ == "__main__":
-    # pretoken_counter = Counter()
-    # pretoken_counter.update(["low", "low", "low", "low", "low", "lower", "lower", "widest", "widest", "widest", "newest", "newest", "newest", "newest", "newest", "newest"])
-
-    # merge_manager = MergeManager(pretoken_counter)
-    # (vocab, merges) = build_vocab(merge_manager, 256 + 9, [])
-    # print(vocab)
-    # print(merges)
-
-    import time
-
+def train_bpe_tinystories():
     start_time = time.perf_counter()
 
     # --- Code you want to measure starts here ---
@@ -139,3 +168,65 @@ if __name__ == "__main__":
     execution_time = end_time - start_time
 
     print(f"Execution time: {execution_time:.6f} seconds")
+
+    with open("data/BPE-TinyStoriesV2-GPT4.pkl", "wb") as f:
+        pickle.dump((vocab, merges), f)
+
+
+def train_bpe_expts_owt():
+    start_time = time.perf_counter()
+
+    # --- Code you want to measure starts here ---
+    (vocab, merges) = train_bpe(
+        input_path="data/owt_valid.txt",
+        vocab_size=32000,
+        special_tokens=["<|endoftext|>"]
+    )
+    # --- Code you want to measure ends here ---
+
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+
+    print(f"Execution time: {execution_time:.6f} seconds")
+
+    with open("data/BPE-owt.pkl", "wb") as f:
+        pickle.dump((vocab, merges), f)
+
+
+def exam_bpe_tinystories():
+    with open("data/BPE-TinyStoriesV2-GPT4.pkl", "rb") as f:
+        (vocab, _) = pickle.load(f)
+
+    # Sorts the items in descending order by length and grabs the first 10
+    longest_10_items = sorted(
+        vocab.items(), key=lambda x: len(x[1]), reverse=True)[:10]
+    print(longest_10_items)
+
+
+def exam_bpe_owt():
+    with open("data/BPE-owt.pkl", "rb") as f:
+        (vocab, _) = pickle.load(f)
+
+    # Sorts the items in descending order by length and grabs the first 10
+    longest_10_items = sorted(
+        vocab.items(), key=lambda x: len(x[1]), reverse=True)[:10]
+    print(longest_10_items)
+
+
+def test_bpe_minimum_case():
+    pretoken_counter = Counter()
+    pretoken_counter.update(["low", "low", "low", "low", "low", "lower", "lower", "widest",
+                            "widest", "widest", "newest", "newest", "newest", "newest", "newest", "newest"])
+
+    merge_manager = MergeManager(pretoken_counter)
+    (vocab, merges) = build_vocab(merge_manager, 256 + 9, [])
+    print(vocab)
+    print(merges)
+
+
+if __name__ == "__main__":
+    # test_bpe_minimum_case()
+    # train_bpe_tinystories()
+    # exam_bpe_tinystories()
+    train_bpe_expts_owt()
+    # exam_bpe_owt()
